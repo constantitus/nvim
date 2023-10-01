0e7b9e611ea82f336c0ed79917869d73e1b496fa
constantitus <constantitus@gmail.com>
2023-10-01 14:50:00 +0300

    idk man

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -20,7 +20,7 @@ return require('packer').startup(function(use)
             }
         end
     }
-    use('github/copilot.vim')
+    -- use('github/copilot.vim')
     use('rcarriga/nvim-notify')
 
     -- navigation
--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -32,7 +32,6 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
-
 vim.keymap.set("n", "Q", "<nop>") -- fuck that
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
