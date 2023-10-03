fa139c7e933b5b932f0fae19f41cd56ee6428ec0
constantitus <constantitus@gmail.com>
2023-10-04 00:04:21 +0300

    offset

--- after/plugin/hop.lua
+++ after/plugin/hop.lua
@@ -1,8 +1,8 @@
 local hop = require('hop')
 local directions = require('hop.hint').HintDirection
 vim.keymap.set('n', '<leader>f', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = 1 })
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = 0 })
 end, {remap=true})
 vim.keymap.set('n', '<leader>F', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 })
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 0 })
 end, {remap=true})
