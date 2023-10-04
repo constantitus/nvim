79595002520f0f6d094b849a645eccd69f8612fc
constantitus <constantitus@gmail.com>
2023-10-04 19:57:12 +0300

    disabling mouse and arrows

--- init.lua
+++ init.lua
@@ -1,5 +1,11 @@
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+-- disabling the mouse and the arrows
+vim.opt.mouse = ""
+vim.keymap.set({'n', 'i', 'x'}, '<Left>', '<Nop>')
+vim.keymap.set({'n', 'i', 'x'}, '<Right>', '<Nop>')
+vim.keymap.set({'n', 'i', 'x'}, '<Up>', '<Nop>')
+vim.keymap.set({'n', 'i', 'x'}, '<DOwn>', '<Nop>')
 require("user")
 
