546e6b7a0b1bdd696971cb25b7a73d0bfd2f206f
constantitus <constantitus@gmail.com>
2023-10-08 16:13:49 +0300

    refactored keymaps

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -1,12 +1,11 @@
 -- set <leader> to space
 vim.g.mapleader = " "
 
--- exit to directory
--- disabled, using tree
--- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- move selection into find and replace
 vim.keymap.set("v", "<leader>r", [["qy:%s/<C-r>q//g<left><left>]])
+-- word find and replace
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- moving selected items with J K
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -21,32 +20,28 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever
+-- Paste without yanking
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+-- Delete without yanking
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- Yank into +
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-
--- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
-
 vim.keymap.set("n", "Q", "<nop>") -- fuck that
--- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
--- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<leader>K", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<leader>J", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader><C-f>", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>k", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>K", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>J", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeToggle<cr>")
 
+-- commands
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>wr", "<cmd>set wrap!<cr>")
-
--- vim.keymap.set("n", "<leader><leader>", function() vim.cmd("so") end)
