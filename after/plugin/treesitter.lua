09715fd035c2716b5b20b81dd774e06a6e73e908
constantitus <constantitus@gmail.com>
2023-10-16 21:09:38 +0300

    formatted code

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,14 @@
 require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query", "rust" },
+    ensure_installed = {
+        "javascript",
+        "typescript",
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "rust"
+    },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
