f8541725221e91368c54bf4fb3bb7173ec80cc44
constantitus <constantitus@gmail.com>
2023-10-16 23:26:06 +0300

    added <leader>x keymap

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -22,6 +22,7 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- Yank into +
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>x", [["+x]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- centers cursor
