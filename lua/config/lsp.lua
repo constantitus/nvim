be340a7f819af557facad7188d13fcbc2dd1105b
constantitus <constantitus@gmail.com>
2023-10-22 22:30:15 +0300

    whoopsie

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -42,10 +42,10 @@ return function()
         },
     })
 
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with({
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
         vim.lsp.handlers.hover,
-        border = "rounded"
-    })
+        {border = "rounded"}
+    )
     vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
         vim.lsp.diagnostic.on_publish_diagnostics, {
             virtual_text = true
