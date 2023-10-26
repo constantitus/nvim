c5d5a0ea8696dc52ee204675a2dc0171bdb100b3
constantitus <constantitus@gmail.com>
2023-10-26 19:11:17 +0300

    added live_grep keymap

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -85,6 +85,7 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>pg", builtin.git_files, {})
 vim.keymap.set("n", "<leader>pu", "<cmd>Telescope undo<cr>")
+vim.keymap.set("n", "<leader>pp", "<cmd>Telescope live_grep<cr>")
 vim.keymap.set("n", "<leader>ps", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
