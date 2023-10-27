ad7a33b50808e8f8d5bed86fc93ea62b69076dc1
constantitus <constantitus@gmail.com>
2023-10-27 17:32:57 +0300

    testing

--- lua/config/noice.lua
+++ lua/config/noice.lua
@@ -1,16 +1,15 @@
 require("noice").setup({
     lsp = {
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
+        --[[ override = {
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
             ["vim.lsp.util.stylize_markdown"] = true,
             ["cmp.entry.get_documentation"] = true,
-        },
+        }, ]]
         hover = { enabled = false, },
     },
     -- you can enable a preset for easier configuration
     presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
         command_palette = true, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
