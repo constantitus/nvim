5eb23ed6c52853a8814737c52382cb23a42b6b7a
constantitus <constantitus@gmail.com>
2023-10-28 15:32:40 +0300

    make <Esc> work in terminal

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -21,3 +21,9 @@ require("toggleterm").setup({
         },
     },
 })
+-- makes <Esc> work in terminal
+function _G.set_terminal_keymaps()
+    local opts = {buffer = 0}
+    vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
+end
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
