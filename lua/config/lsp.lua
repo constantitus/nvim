8ee85e835d349f60be6f8a5dc25ebdaf0b63d491
constantitus <constantitus@gmail.com>
2023-10-28 23:19:21 +0300

    changed lsp keymaps to start with <leader>l

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -8,7 +8,7 @@ return function()
         vim.keymap.set("n", "gd", function() -- definition
             vim.api.nvim_feedkeys("mB", "n", false)
             vim.lsp.buf.definition()
-        end, opts )
+        end, opts)
         vim.keymap.set("n", "gD", function() -- declaration
             vim.api.nvim_feedkeys("mB", "n", false)
             vim.lsp.buf.declaration()
@@ -18,16 +18,16 @@ return function()
             vim.lsp.buf.implementation()
         end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.lsp.buf.open_float() end, opts)
         vim.keymap.set("n", "]d", function() vim.lsp.buf.goto_next() end, opts)
         vim.keymap.set("n", "[d", function() vim.lsp.buf.goto_prev() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("n", "<leader>vh", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.lsp.buf.open_float() end, opts)
+        vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>lrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>lrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("n", "<leader>lh", function() vim.lsp.buf.signature_help() end, opts)
         vim.keymap.set("n", "<leader><C-f>", vim.lsp.buf.format)
-        vim.keymap.set("n", "<leader>l", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>k", function() vim.diagnostic.open_float() end, opts)
     end)
 
     vim.diagnostic.config({
@@ -44,7 +44,7 @@ return function()
 
     vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
         vim.lsp.handlers.hover,
-        {border = "rounded"}
+        { border = "rounded" }
     )
     vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
         vim.lsp.diagnostic.on_publish_diagnostics, {
