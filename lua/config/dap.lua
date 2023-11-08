4628214f6368cfac7514f679c1a9da186360f79d
constantitus <constantitus@gmail.com>
2023-11-09 01:04:54 +0200

    dap keymaps

--- lua/config/dap.lua
+++ lua/config/dap.lua
@@ -5,3 +5,7 @@ require("neodev").setup({
   library = { plugins = { "nvim-dap-ui" }, types = true },
 })
 vim.keymap.set("n", "<leader>dx", "<Cmd>DapTerminate<CR>", {noremap=true})
+vim.keymap.set("n", "<F5>", "lua require'dap'.continue()<CR>")
+vim.keymap.set("n", "<F10>", "lua require'dap'.step_over()<CR>")
+vim.keymap.set("n", "<F11>", "lua require'dap'.step_into()<CR>")
+vim.keymap.set("n", "<F12>", "lua require'dap'.step_out()<CR>")
