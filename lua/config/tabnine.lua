fac8292df62ce3354c7659a4d79a58a2381f4de4
constantitus <constantitus@gmail.com>
2023-11-22 17:18:59 +0200

    fixed some tabnine errors

--- lua/config/tabnine.lua
+++ lua/config/tabnine.lua
@@ -1,10 +1,10 @@
 require('tabnine').setup({
   disable_auto_comment=true,
-  accept_keymap="<CR>", -- "<CR>",
+  accept_keymap="<S-CR>", -- "<CR>",
   -- dismiss_keymap = "",
   debounce_ms = 800,
   suggestion_color = {gui = "#808080", cterm = 244},
-  exclude_filetypes = {"TelescopePrompt"},
+  exclude_filetypes = {"TelescopePrompt", "oil", "response"},
   log_file_path = nil, -- absolute path to Tabnine log file
 })
 
