da087edeb3a9b726404d7c734fec65111b82f865
constantitus <constantitus@gmail.com>
2023-12-02 02:12:06 +0200

    configured noice

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -37,6 +37,7 @@ vim.diagnostic.config({
     },
 })
 
+--[[ -- handled by noice.nvim
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
     vim.lsp.handlers.hover,
     { border = "rounded" }
@@ -44,7 +45,7 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
     vim.lsp.handlers.signature_help,
     { border = "rounded" }
-)
+) ]]
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
         virtual_text = true,
--- lua/config/noice.lua
+++ lua/config/noice.lua
@@ -6,17 +6,22 @@ require("noice").setup({
             ["vim.lsp.util.stylize_markdown"] = true,
             ["cmp.entry.get_documentation"] = true,
         },
-        hover = { enabled = false, },
-        signature = { enabled = false, },
     },
     -- you can enable a preset for easier configuration
     presets = {
         bottom_search = true,
-        command_palette = false,      -- position the cmdline and popupmenu together
+        command_palette = true,      -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-        lsp_doc_order = false,        -- add a border to hover docs and signature help
+        inc_rename = true,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true,        -- add a border to hover docs and signature help
     },
+    views = {
+        cmdline_popup = {
+            position = {
+                row = "50%"
+            }
+        }
+    }
 })
 require("notify").setup({
     background_colour = "#000000",
